eb59384247fe84bfa0d18444299ac60f377662a9
LaxrLee <stangathagu95@gmail.com>
2022-03-10 05:13:14 +0300

    async

--- drusk-backend/src/base/data/mongo-query-builder/abstract.repository.ts
+++ drusk-backend/src/base/data/mongo-query-builder/abstract.repository.ts
@@ -5,4 +5,22 @@ import { MongoObject } from './mongo-object.interface';
 /*
 ~ Abstract repository that shares common repository functionalities
 */
-export abstract class
+export abstract class AbstractRepository<T extends Mongobjects> {
+    constructor(private readonly _model:any) { }
+
+    async findAll(): Promise<T[]> {
+        return this.findAll({});
+    }
+
+    async trySingle(query: any): Promise<T | false> {
+        let res = await this.find(query);
+
+        if (res.length > 0) return res.shift();
+        else return false;
+    }
+
+    async findById(id: ObjectId): Promise<T> {
+        let res = await this.findAll({_id: id });
+        return res,shift();
+    }
+}
