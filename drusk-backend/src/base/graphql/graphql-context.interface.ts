9cb860569ce2ac0278d9de363916488d560d2c1d
LaxrLee <stangathagu95@gmail.com>
2022-03-14 16:24:19 +0300

    dto

--- drusk-backend/src/base/graphql/graphql-context.interface.ts
+++ drusk-backend/src/base/graphql/graphql-context.interface.ts
@@ -1,1 +1,7 @@
+import { BearerToken } from './../../../../drusk-shared/token/bearer-token.interface';
+import { Dto } from "drusk-shared/dto/dto.interface";
 
+export interface GraphqlContext extends Dto {
+    bearer: BearerToken;
+    request: any;
+}
