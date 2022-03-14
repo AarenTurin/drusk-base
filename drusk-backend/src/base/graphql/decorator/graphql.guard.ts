40d55eb67ee72ac750892631420f290519826822
LaxrLee <stangathagu95@gmail.com>
2022-03-14 15:19:25 +0300

    endpoint protection

--- drusk-backend/src/base/graphql/decorator/graphql.guard.ts
+++ drusk-backend/src/base/graphql/decorator/graphql.guard.ts
@@ -1,42 +1,52 @@
 import { ReflectMetadata } from "@nestjs/common";
-import { BeaerToken } from "elewa-shared/token/bearer-token.interface";
+import { BearerToken } from "elewa-shared/token/bearer-token.interface";
 import _ = require("underscore");
-import { _Field } from "graphlq";
+import { __Field } from "graphql";
 
 /**
- * Custom Decorator that enables a resolver to execue a query
- * Note: Only use this for queries, not for resolveProperty
- * Inspired by  https://github.com/nestjs/graphql/blob/master/lib/decorators/resolvers.decorators.ts
+ * Custom Decorator that enables a resolver to execute a query.
+ *
+ *  Note: Only use this for Queries, not for ResovleProperty!
+ *
+ * Inspired by https://github.com/nestjs/graphql/blob/master/lib/decorators/resolvers.decorators.ts
  */
+export function createGraphqlGuard(name: string) {
+  return function (resolver?: string)
+  {
+    return function (
+      target: Object,
+      key: string | symbol,
+      descriptor: TypedPropertyDescriptor<Function>
+    ) {
+      // 1. Set the Graphql Resolver Type and name - See Nest Query-Resolver.
+      ReflectMetadata("graphql:resolver_type", resolver || name)(
+        target,
+        key,
+        descriptor
+      );
+      ReflectMetadata("graphql:resolver_name", name)(target, key, descriptor);
 
-export function createGraphGuard(name: string) {
-    return function (resolver?: string)
-    {
-        return function (
-            target: Object,
-            key: string | symbol,
-            descriptor: TypedPropertyDescriptor<Function>
-        ){
-            // set the Graph1 resolver type and name - see nest query-resolver
-            ReflectMetaData("graphql:resolver_type", resolver || name)(
-                target,
-                key,
-                descriptor
-            );
-            ReflectMetadata("graphql:resolver_type", resolver || name)(
-                target,
-                key,
-                descriptor
-            );
-            ReflectMetadata("graphql:esolver_name", name)(target, key, descriptor);
+      const toExecute = descriptor.value;
+      // 1. Change the Descriptor Value to Reflect the changes
+      descriptor.value = async function (...args: any[]) {
+        console.log("Graphql ClaimsGaurd: Checking if User is Authorised.");
 
-            const toExecute = descriptor.value;
-            // Change the descriptor value to reflect the changes
-            descriptor.value = async function (...args: any[]) {
-                console.log("Graphql ClaimsGaurd: Checking if User is Authorised.");
+        // Get the context from the parameters.
+        args[2].bearer = await args[2].bearer;
+        // Get code structures we need to reflect on - The ones annotated with RequireClaims.
+        _claimsGaurd(args[2].bearer, target.constructor, target[key]);
 
-            //Get the ontext from the parameters
-            }
-        }
-    }
-}
+        return toExecute.apply(this, args);
+      };
+    };
+  }
+
+  /** protects endpoint from unauthored requests */
+  fucntion _claimsGaurd(bearer: BearerToken, classTarget, methodTarget) {
+      const neededClaims = _getNeededClaims(classTarget, methodTarget);
+
+      if (_tokenHasClaims(bearer, <string[]>neededClaims)) return true;
+      else throw new Error("Forbudden resource");
+  }
+
+  
