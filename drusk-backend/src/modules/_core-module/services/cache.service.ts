05478be75de52c4892264f507a9b9e4ed3482935
LaxrLee <stangathagu95@gmail.com>
2022-03-14 17:09:32 +0300

    cache services

---
+++ drusk-backend/src/modules/_core-module/services/cache.service.ts
@@ -0,0 +1,48 @@
+import * as cache from 'memory-cache';
+import { Component } from '@nestjs/common';
+
+/**
+ * cache component that can store and retriece data
+ * 
+ * Uses https://github.com/ptarjan/node-cache internally
+ */
+
+@Component()
+export class CacheService {
+    /**Create the cache on server load */
+    private readonly _cache = new cache.Cache();
+
+    /** 
+     * Simply stores a value and returns the cached value
+     * 
+     * @param key
+     * @param value
+     * @param time will remove the value in the specified time in seconds. Optional, if time isn't passed in it is stored forever.
+     * @param callback callback is optional function fires after entry has expired with key and value passed (function(key, value) {})
+     */
+    put(key :string, value: any, time?: number, callback?:(string, any) => void) : any
+    {
+        //Uses time in ms, we in s
+        this._cache.put(key, value, time * 1000, callback);
+    }
+
+    /**
+     * Retrieves a value for a given key
+     * if value isn't cached, returns null
+     * 
+     * @param key
+     */
+    get(key: string) : any | null
+    {
+        return this._cache.get(key);
+    }
+
+    /**
+     * Deletes a key, returns a boolean specifying whether or not the key was deleted
+     * 
+     * @param key
+     */
+    delete(key): boolean {
+        return this._cache.del(key);
+    }
+}
