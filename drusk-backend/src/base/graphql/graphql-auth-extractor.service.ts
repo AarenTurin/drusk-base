734bd62c23c2dbcda12f5ff5f15e638f97c75f95
LaxrLee <stangathagu95@gmail.com>
2022-03-14 16:08:00 +0300

    initialize

---
+++ drusk-backend/src/base/graphql/graphql-auth-extractor.service.ts
@@ -0,0 +1,4 @@
+import * as jwt from "jsonwebtoken";
+import { BearerToken } from "drusk-shared/token/bearer-token.interface";
+import { Component } from "@nestjs/common";
+import { AuthConfigService } from "../../modules/auth/services/auth-config.service";
