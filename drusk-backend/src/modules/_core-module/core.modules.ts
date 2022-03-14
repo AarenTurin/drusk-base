e18ca07b75ad18758ff07222f35e64e5c52ac465
LaxrLee <stangathagu95@gmail.com>
2022-03-14 17:13:53 +0300

    core

---
+++ drusk-backend/src/modules/_core-module/core.modules.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { CacheService } from './services/cache.service';
+
+@Module({
+    components: [CacheService],
+    exports: [CacheService]
+})
+export class CoreModule { }
