d4e3e5f302c75c473f0dd3305fc5f3528549207d
LaxrLee <stangathagu95@gmail.com>
2022-03-14 17:27:41 +0300

    boot service

---
+++ drusk-backend/src/modules/_server-boot/sever-boot.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { MpngooseModule } from '@nestjs/mongoose';
+
+import { CurriculumModule } from '../curriculum/curriculum.module';
+
+import { BootService } from './boot.service';
+import { AuthModule } from '../auth/auth.module';
+import { BootAuthService } from './module-boots/boot-auth.service';
+import { BootCurriculumService } from './module-boots/boot-curriculum.service';
+import { ConfigureCorsService } from './configure-server/configure-cors.service';
+import { ConfigureAuthService } from './configure-server/cofigure-auth.service';
+
+
+@Module({
+    imports: [CurriculumModule, AuthModule],
+    controllers: [],
+    components: [ConfigureCorsService, ConfigureAuthService,
+                 BootAuthService, BootCurriculumService,
+                
+                 BootService],
+})
+export class ServerBootModule {}
